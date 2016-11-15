6e1c20c43c71e6e8a703deb52f1fa59bd0739b94
mraimbot <mraimbot@hotmail.de>
2016-11-15 18:41:04 +0100

    Update 0.1.0 + added TileMap.cs + added Entity.cs     + added Enemy.cs     + added Player.cs

---
+++ Rage of Stickman/TileMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rage_of_Stickman
+{
+	class TileMap
+	{
+		public TileMap(Texture2D map)
+		{
+			// TODO TileMap - TileMap() - load picture
+			Array <-- tile-ID <-- color <-- points[] <-- picture
+		}
+
+		public Draw()
+		{
+
+		}
+	}
+}
